69a197b178fa81274b00e73f2020c34c76308611
keipy27 <kevinandris27@gmail.com>
2023-08-13 10:25:55 +1200

    utils folder created and dependencies

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,3 +1,6 @@
+// ! ROOT CLASS
+//  ! npm create vite@latest to create the frontend
+// ! npm install and npm run dev
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
