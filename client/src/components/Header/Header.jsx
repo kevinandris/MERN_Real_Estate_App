6c80daf01f34d12a739f824a997952f196f58744
keipy27 <kevinandris27@gmail.com>
2023-08-13 10:55:22 +1200

    Header section completed

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -3,7 +3,23 @@ import './Header.css'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <section className='h-wrapper'>
+
+            <div className="paddings innerWidth h-container">
+                <img src="./logo.png" alt="logo" width={100}/>
+                
+                <div className="flexCenter h-menu">
+                    <a href="">Residencies</a>
+                    <a href="">Our Value</a>
+                    <a href="">Contact Us</a>
+                    <a href="">Get Started</a>
+                    <button className='button'>
+                        <a href="">Contact</a>
+                    </button>
+                </div>
+                
+            </div>
+    </section>
   )
 }
 
