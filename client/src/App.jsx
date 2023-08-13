1b11495821c8f40a34b8b15989fb0f2a49b7e33c
keipy27 <kevinandris27@gmail.com>
2023-08-13 12:14:22 +1200

    companies section is implemented

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from './components/Header/Header'
 import Hero from './components/Hero/Hero'
 import './App.css'
+import Companies from './components/Companies/Companies'
 
 function App() {
 
@@ -12,6 +13,8 @@ function App() {
             <Header />
             <Hero />
           </div>
+
+          <Companies />
       </div>
   )
 }
