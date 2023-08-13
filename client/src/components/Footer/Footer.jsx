eaca6a59104bd3610e89b7e09a4526aa2a5e8a63
keipy27 <kevinandris27@gmail.com>
2023-08-13 22:47:04 +1200

    Footer section completed

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -13,6 +13,19 @@ const Footer = () => {
                     the best place to live for them.
                 </span>
             </div>
+
+            {/* // ! right side */}
+            <div className="flexColStart f-right">
+                <span className='primaryText'>Information</span>
+                <span>13 Canterbury Park Lane, Auckland, NZ</span>
+
+                <div className="flexCenter f-menu">
+                    <span>Property</span>
+                    <span>Services</span>
+                    <span>Product</span>
+                    <span>About Us</span>
+                </div>
+            </div>
         </div>
     </section>
   )
