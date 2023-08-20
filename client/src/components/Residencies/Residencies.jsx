e3f7f79454dc3c42293fd105e7356558862a78ae
keipy27 <kevinandris27@gmail.com>
2023-08-20 12:08:49 +1200

    adding a slice method to only show 8 residencies for the slider part

--- client/src/components/Residencies/Residencies.jsx
+++ client/src/components/Residencies/Residencies.jsx
@@ -3,33 +3,58 @@ import React from 'react'
 import './Residencies.css'
 import { Swiper, SwiperSlide, useSwiper } from 'swiper/react'
 import "swiper/css"
-import data from '../../utils/slider.json'
 import { sliderSettings } from '../../utils/common.js'
 import PropertyCard from '../PropertyCard/PropertyCard'
+import { PuffLoader } from 'react-spinners'
 
 const Residencies = () => {
-  return (
-    <section className='r-wrapper'>
-        <div className="paddings innerWidth r-container">
-            <div className="r-head flexColStart">
-                <span className='orangeText'>Best Choices</span>
-                <span className='primaryText'>Popular Residencies</span>
-            </div>
+    const {data, isError, isLoading} = useProperties()
 
-            <Swiper {...sliderSettings}>
-                <SlideNextButton />
-                {/* // ! slider */}
-                {
-                    data.map((card, index) => (
-                        <SwiperSlide key={index}>
-                            <PropertyCard card={card} />
-                        </SwiperSlide>
-                    ))
-                }
-            </Swiper>
+      /* //! ERROR STATE */
+    if (isError) {
+        return (
+        <div className="wrapper">
+            <span>Error while fetching data</span>
         </div>
-    </section>
-  )
+        )
+    } 
+
+    /* //! LOADING STATE */
+    if (isLoading) {
+        return (
+        <div style={{height:"60vh"}} className="wrapper flexCenter">
+            <PuffLoader 
+                height="80" 
+                width="80" 
+                radius={1} 
+                color='#4066ff' 
+                aria-label="puff-loading"/>
+        </div>
+        )
+    }
+
+    return (
+        <section className='r-wrapper'>
+            <div className="paddings innerWidth r-container">
+                <div className="r-head flexColStart">
+                    <span className='orangeText'>Best Choices</span>
+                    <span className='primaryText'>Popular Residencies</span>
+                </div>
+
+                <Swiper {...sliderSettings}>
+                    <SlideNextButton />
+                    {/* // ! slider */}
+                    {
+                        data.slice(0, 8).map((card, index) => (
+                            <SwiperSlide key={index}>
+                                <PropertyCard card={card} />
+                            </SwiperSlide>
+                        ))
+                    }
+                </Swiper>
+            </div>
+        </section>
+    )
 }
 
 export default Residencies;
