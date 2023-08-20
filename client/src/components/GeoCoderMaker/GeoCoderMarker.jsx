c6f8e48d50db8dfa9aefce7083fb29de6fa5d35a
keipy27 <kevinandris27@gmail.com>
2023-08-21 10:57:04 +1200

    GeoCodermaker.jsx file is implemented to fetch the location of the house in the map using esri-leaflet-geocoder dependency

--- client/src/components/GeoCoderMaker/GeoCoderMarker.jsx
+++ client/src/components/GeoCoderMaker/GeoCoderMarker.jsx
@@ -1,14 +1,33 @@
-import React, { useState } from 'react'
-import { Marker, Popup } from 'react-leaflet'
+import React, { useEffect, useState } from 'react'
+import { Marker, Popup, useMap } from 'react-leaflet'
 import L from 'leaflet'
 import "leaflet/dist/leaflet.css"
 import icon from "leaflet/dist/images/marker-icon.png"
 import iconShadow from "leaflet/dist/images/marker-shadow.png"
+import * as ELG from 'esri-leaflet-geocoder'
+
+let DefaultIcon = L.icon ({
+    iconUrl :icon,
+    shadowUrl: iconShadow
+})
+
+L.Marker.prototype.options.icon = DefaultIcon
 
 const GeoCoderMarker = ({address}) => {
 
     const map = useMap()
     const [position, setPosition] = useState([60, 19])
+
+    useEffect(() => {
+        /* translate our address into our map*/
+        ELG.geocode().text(address).run((err, result, response) => {
+            if (results?.results?.length > 0) {
+                const {lat, lng} = results?.results[0].latlng
+                setPosition([lat, lng])
+                map.flyTo([lat, lng], 6)
+            }
+        })
+    }, [address])
 
     return (
         <Marker position={position} icon={DefaultIcon}>
