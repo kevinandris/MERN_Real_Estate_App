5a674d45493a7b120c41b7bbe87cff47bb9929bc
keipy27 <kevinandris27@gmail.com>
2023-08-20 12:36:28 +1200

    mapping the images in property.jsx and style the page for like and images imported from the data

--- client/src/pages/Property/Property.jsx
+++ client/src/pages/Property/Property.jsx
@@ -3,15 +3,45 @@ import './Property.css'
 import { useQuery } from 'react-query'
 import { useLocation } from 'react-router-dom'
 import { getProperty } from '../../utils/api'
+import { PuffLoader } from 'react-spinners'
+import { AiFillHeart } from 'react-icons/ai'
 
 const Property = () => {
     const { pathname } = useLocation()
     const id = pathname.split("/").slice(-1)[0]
     const {data, isLoading, isError} = useQuery(['resd', id], () => getProperty(id))
+
+    if (isLoading) {
+        return (
+            <div className="wrapper">
+                <div className="flexCenter paddings">
+                    <PuffLoader />
+                </div>
+            </div>
+        )
+    }
+
+    if (isError) {
+        return (
+            <div className="wrapper">
+                <div className="flexCenter paddings">
+                    <span>Error while fetching the property details</span>
+                </div>
+            </div>
+        )
+    }
     
     return (
         <div className='wrapper'>
-            This is an individual property path
+            <div className="flexColStart paddings innerWidth property-container">
+                {/* like button */}
+                <div className="like">
+                    <AiFillHeart size={24} color='#fff'/>
+                </div>
+
+                {/* image */}
+                <img src={data.image} alt='home image' />
+            </div>
         </div>
     )
 }
