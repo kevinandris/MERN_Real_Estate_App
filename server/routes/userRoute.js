8b0d13a3c3842cf9235e49179e1836fb3b428f3c
keipy27 <kevinandris27@gmail.com>
2023-08-21 12:29:06 +1200

    adding jwtCheck middleware in userRoute for authentication

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,10 +1,11 @@
 // ! 4
 import express from "express";
 import { bookVisit, createUser, getAllBookings, cancelBooking, toFav, getAllFavorites } from "../controllers/userController.js"
+import jwtCheck from "../config/auth0Config.js";
 
 const router = express.Router();
 
-router.post("/register", createUser)
+router.post("/register", jwtCheck, createUser)
 router.post("/bookVisit/:id", bookVisit)
 router.post("/allBookings", getAllBookings)
 router.post("/removeBookings/:id", cancelBooking)
