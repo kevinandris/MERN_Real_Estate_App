4ec77ec981782d09f93264b47f6e3c05aa90789d
keipy27 <kevinandris27@gmail.com>
2023-08-21 14:59:16 +1200

    handleBookingSuccess function is implemented to show a successful message using toastify

--- client/src/components/BookingModal/BookingModal.jsx
+++ client/src/components/BookingModal/BookingModal.jsx
@@ -3,15 +3,24 @@ import { Modal } from '@mantine/core'
 import { DatePicker } from '@mantine/dates'
 import { useMutation } from 'react-query'
 import UserDetailContext from '../context/UserDetailsContext'
+import { bookVisit } from '../../utils/api'
+import { toast } from 'react-toastify'
 
 const BookingModal = ({opened, setOpened, email, propertyId}) => {
 
   const [value, setValue] = useState(null)
   const { userDetails : {token}} = useContext(UserDetailContext)
+  const handleBookingSuccess = () => {
+    toast.success("You have booked your visit", {
+      position: "bottom-right"
+
+    })
+  }
 
   const {mutate, isLoading} = useMutation( {
-    mutationFn: () => bookVisit(value, propertyId, email, token)
-  })
+    mutationFn: () => bookVisit(value, propertyId, email, token),
+    onSuccess: () => handleBookingSuccess()
+  });
   
   return (
     <Modal
@@ -23,7 +32,7 @@ const BookingModal = ({opened, setOpened, email, propertyId}) => {
         <div className='flexColCenter'>
           <DatePicker value={value} onChange={setValue} minDate={new Date()}/>
 
-          <button disabled={!value} onClick={() => mutate()}>
+          <button disabled={!value || isLoading} onClick={() => mutate()}>
               Book Visit
           </button>
         </div>
