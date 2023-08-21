e7f2be3e065910a9c907de1a830375bf0e5d20ae
keipy27 <kevinandris27@gmail.com>
2023-08-21 12:15:44 +1200

    adding authentication function with useEffect in Layout.jsx

--- client/src/components/Layout/Layout.jsx
+++ client/src/components/Layout/Layout.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
 import Header from '../Header/Header.jsx'
 import Footer from '../Footer/Footer.jsx'
 import { Outlet } from 'react-router-dom'
+import { useAuth0 } from '@auth0/auth0-react'
+import UserDetailContext from '../context/UserDetailsContext.js'
+import { useMutation } from 'react-query'
 
 const Layout = () => {
+
+  const {isAuth, user} = useAuth0()
+  const {setUserDetails} =  useContext(UserDetailContext)
+
+  const {mutate} = useMutation ({
+    mutationKey: [user?.email],
+    mutationFn: () => createUser(user?.email)
+  })
+
+  useEffect(() => {
+
+  }, [isAuthenticated])
   return (
+
     <>
         <div style={{background: "var(--black)", overflow: "hidden"}}>
             <Header/>
