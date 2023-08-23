7ff49ecf70ad8dc5324d089dce1a62ba465becf2
keipy27 <kevinandris27@gmail.com>
2023-08-23 13:34:52 +1200

    added Box, form, texInput, Textarea and NumberInput from mantine core and form inside BasicDetails.jsx

--- client/src/components/BasicDetails/BasicDetails.jsx
+++ client/src/components/BasicDetails/BasicDetails.jsx
@@ -1,21 +1,23 @@
 // ! exported to AddPropertyModal.jsx
 import React from 'react'
 import { validateString } from '../../utils/common';
+import { Box, Button, Group, NumberInput, TextInput, Textarea } from '@mantine/core';
+import { Form } from '@mantine/form';
 
-const BasicDetails = (title, description, price) => {
+const BasicDetails = ({ prevStep, nextStep, propertyDetails, setPropertyDetails }) => {
 
     const form = useForm({
         initialValues: {
-            title: propertyDetails?.title,
-            description: propertyDetails?.description,
-            price: propertyDetails?.price
+            title: propertyDetails.title,
+            description: propertyDetails.description,
+            price: propertyDetails.price
         },
 
         validate: {
             title: (value) => validateString(value),
             description: (value) => validateString(value),
             price: (value) => 
-                value <= 1000 ? "Must be greater than 999 dollars" : null,
+                value < 1000 ? "Must be greater than 999 dollars" : null,
         }
     })
 
@@ -31,7 +33,45 @@ const BasicDetails = (title, description, price) => {
     }
 
   return (
-    <div>BasicDetails</div>
+    <Box>
+        <Form
+            onSubmit={(e) => {
+                e.preventDefault();
+                handleSubmit()
+            }}
+        >
+            <TextInput 
+                withAsterisk
+                label="Title"
+                placeholder='Property Name'
+                {...form.getInputProps("title")}
+            />
+            
+            <Textarea 
+                 withAsterisk
+                placeholder='Description'
+                label="Description"
+                {...form.getInputProps("description")}
+            />
+
+            <NumberInput 
+                withAsterisk
+                label="Price"
+                placeholder='1000'
+                min={0}
+                {...form.getInputProps("price")}
+            />
+
+            <Group position='center' mt={'xl'}>
+                <Button variant='default' onClick={prevStep}>
+                    Back
+                </Button>
+                <Button type='submit'>
+                    Next step
+                </Button>
+            </Group>
+        </Form>
+    </Box>
   )
 }
 
