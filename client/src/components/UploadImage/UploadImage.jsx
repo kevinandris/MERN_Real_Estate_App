2de7f2b5689fe1224a9e6ad6d13e202bf5c55957
keipy27 <kevinandris27@gmail.com>
2023-08-23 12:21:53 +1200

    imageURL, cloudinaryRef and widgetRef as well as useEffect are implemented

--- client/src/components/UploadImage/UploadImage.jsx
+++ client/src/components/UploadImage/UploadImage.jsx
@@ -1,18 +1,38 @@
 // ! exported to AddPropertyModal.jsx
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { AiOutlineCloudUpload } from 'react-icons/ai'
 import './UploadImage.css'
 
 const UploadImage = ({propertyDetails, setPropertyDetails, nextStep, prevStep}) => {
 
     const [ imageURL, setImageURL] = useState(propertyDetails.image)
+    const cloudinaryRef = useRef()
+    const widgetRef = useRef()
+
+    useEffect(() => {
+        cloudinaryRef.current = window.cloudinary;
+        widgetRef.current = cloudinaryRef.current.createUploadWidget(
+            {
+                cloudName: "daz6x21s0",
+                uploadPreset: "a2zabhnh",
+                maxFile: 1
+            },
+            (err, result) => {
+                if (result.event === "success") {
+                    setImageURL(result.info.secure_url)
+                }
+            }
+        )
+    })
 
     return (
 
         <div className="flexColCenter uploadWrapper">
             {
                 !imageURL ? (
-                    <div className="flexColCenter uploadZone">
+                    <div className="flexColCenter uploadZone"
+                        
+                    >
                         <AiOutlineCloudUpload size={50} color='grey' />
                         <span>Upload Image</span>
                     </div>
