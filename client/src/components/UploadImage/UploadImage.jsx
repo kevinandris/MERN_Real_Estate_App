2bc7bd273d6a86b6f6c8a4b3b1f045a664850fbb
keipy27 <kevinandris27@gmail.com>
2023-08-23 12:11:22 +1200

    UploadImage.jsx c ontains elements such as an icon, words and imgURL

--- client/src/components/UploadImage/UploadImage.jsx
+++ client/src/components/UploadImage/UploadImage.jsx
@@ -1,14 +1,28 @@
 // ! exported to AddPropertyModal.jsx
-import React from 'react'
+import React, { useState } from 'react'
+import { AiOutlineCloudUpload } from 'react-icons/ai'
 
 const UploadImage = ({propertyDetails, setPropertyDetails, nextStep, prevStep}) => {
-  return (
-    <div className="flexColCenter uploadWrapper">
-        {
-            
-        }
-    </div>
-  )
+
+    const [ imageURL, setImageURL] = useState(propertyDetails.image)
+
+    return (
+
+        <div className="flexColCenter uploadWrapper">
+            {
+                !imageURL ? (
+                    <div className="flexColCenter uploadZone">
+                        <AiOutlineCloudUpload size={50} color='grey' />
+                        <span>Upload Image</span>
+                    </div>
+                ) : (
+                    <div className="uploadImage">
+                        <img src={imageURL} alt="" />
+                    </div>
+                )
+            }
+        </div>
+    )
 }
 
 export default UploadImage
